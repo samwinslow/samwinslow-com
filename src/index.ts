7efc4c92138098ea8b2f5d63f0b02f6be0e5dc5c
Sam Winslow <4645779+samwinslow@users.noreply.github.com>
2026-07-17 10:14:36 -0700

    content and styling

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log("Hello, TypeScript 6.0.3 with ESNext modules and ES2022 target!");
+window.addEventListener("load", () => {
+  const element = document.getElementById("tel-placeholder");
+
+  if (element) {
+    element.innerHTML = "+1 (" + (424*2) + ") " + (206*2) + "-0" + (341*2);
+    element.setAttribute("href", "tel:+1" + (424*2) + "" + (206*2) + "0" + (341*2))
+  }
+});
